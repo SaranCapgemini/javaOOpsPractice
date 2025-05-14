c1f7783e377d858e99de6fd4b693ae41bbe06d9f
SaranCapgemini <saran.n@capgemini.com>
2025-05-14 10:24:27 +0530

    Completed

--- Book.java
+++ Book.java
@@ -1,3 +1,33 @@
 public class Book{
     private String title;
+    private String author;
+    private float price;
+
+    public void settitle(String title){
+        this.title = title;
+    }
+
+    public void setauthor(String author){
+        this.author = author;
+    }
+
+    public void setprice(float price){
+        this.price = price;
+    }
+
+    public String gettitle(){
+        return title;
+    }
+
+    public String getauthor(){
+        return author;
+    }
+
+    public float getprice(){
+        return price;
+    }
+
+    public String toString(){
+        return The book title is +title+ " written by " + author + " and costs " + price;
+    }
 }
